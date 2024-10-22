4224d9267b496d8179312bd47eee52375cb006aa
cesar muñoz araya <kas.cesar@gmail.com}>
2024-10-22 14:52:25 -0300

    Ahora el usuario pude determinar si instalar el entorno o no
    De todas maneras el entorno crea un readme el cual muestra como
    prepara el entorno.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-export PIPENV_VENV_IN_PROJECT=1
-git config --global init.defaultBranch main
-git init
-pipenv install
-pipenv run dvc init
-pipenv run dvc config core.analytics false
-.venv/bin/pre-commit install
-pipenv run pdoc --mermaid --math {{cookiecutter.folder_name}}/ -o docs/
-git add .
-git commit -m "Project {{cookiecutter.short_title}} creation"
-git add .
-git commit -m "Project {{cookiecutter.short_title}} creation"
+if [[ "{{ cookiecutter.prepare_environment }}" == "y" ]]; then
+    git init
+    pipenv install
+    pipenv run dvc init
+    pipenv run dvc config core.analytics false
+    .venv/bin/pre-commit install
+    pipenv run pdoc --mermaid --math {{cookiecutter.folder_name}}/ -o docs/
+    git add .
+    git commit -m "Project {{cookiecutter.short_title}} creation"
+    git add .
+    git commit -m "Project {{cookiecutter.short_title}} creation"
+else
+    echo "Skipping environment setup."
+fi
